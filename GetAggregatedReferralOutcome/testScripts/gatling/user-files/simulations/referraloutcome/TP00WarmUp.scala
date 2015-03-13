c8e8ad4bfafc2c72befbdc42203fc0bfe5aef3a7
Martin Flower <martin.flower@callistaenterprise.se>
2015-03-13 15:04:23 +0100

    Updated Gatling tests

--- GetAggregatedReferralOutcome/testScripts/gatling/user-files/simulations/referraloutcome/TP00WarmUp.scala
+++ GetAggregatedReferralOutcome/testScripts/gatling/user-files/simulations/referraloutcome/TP00WarmUp.scala
@@ -11,24 +11,23 @@ import scenarios.GetAggregatedReferralOutcomeScenario
  */
 class TP00WarmUp extends Simulation {
 
-
 //val baseURL             = "https://test.esb.ntjp.se/vp/clinicalprocess/healthcond/actoutcome/GetReferralOutcome/3/rivtabp21"
 //val baseURL             = "http://ine-dit-app02.sth.basefarm.net:9014/GetAggregatedReferralOutcome/service/v3"
-val baseURL             = "https://qa.esb.ntjp.se/vp/clinicalprocess/healthcond/actoutcome/GetReferralOutcome/3/rivtabp21"
+  val baseURL             = "https://qa.esb.ntjp.se/vp/clinicalprocess/healthcond/actoutcome/GetReferralOutcome/3/rivtabp21"
 
-  val testDuration      = 60 * 60 * 12 seconds // 12 timmar
-  val minWaitDuration   = 2000 milliseconds
-  val maxWaitDuration   = 5000 milliseconds
-  val times:Int         = 1
+  val testDuration      =  1 minute
+  val minWaitDuration   =  2 seconds
+  val maxWaitDuration   =  5 seconds
+  val times:Int         =  1 // 6
   
   val httpProtocol = http.baseURL(baseURL).disableResponseChunksDiscarding
 
   val warmUp = scenario("warm up")
                  .repeat(times) {
-                   feed(csv("patients.csv").queue)
-//                   exec(session => {
-//                     session.set("status","200").set("patientid","121212121212").set("name","Tolvan Tolvansson").set("count","3")
-//                   })    
+//                   feed(csv("patients.csv").queue)
+                   exec(session => {
+                     session.set("status","200").set("patientid","121212121212").set("name","Tolvan Tolvansson").set("count","3")
+                   })    
                    .exec(GetAggregatedReferralOutcomeScenario.request)
                    .pause(1 second)
                   }
--- GetAggregatedReferralOutcome/testScripts/gatling/user-files/simulations/scenarios/GetAggregatedReferralOutcomePingForConfigurationScenario.scala
+++ GetAggregatedReferralOutcome/testScripts/gatling/user-files/simulations/scenarios/GetAggregatedReferralOutcomePingForConfigurationScenario.scala
@@ -23,35 +23,4 @@ object GetAggregatedReferralOutcomePingForConfigurationScenario {
           .check(xpath("soap:Envelope", List("soap" -> "http://schemas.xmlsoap.org/soap/envelope/")).exists)
           .check(regex("GetAggregatedReferralOutcome").exists)
       )
-  }
-
-
-
-/*
-<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:urn="urn:riv:itintegration:registry:1" xmlns:urn1="urn:riv:itintegration:monitoring:PingForConfigurationResponder:1">
-   <soapenv:Header>
-      <urn:LogicalAddress>5565594230</urn:LogicalAddress>
-   </soapenv:Header>
-   <soapenv:Body>
-      <urn1:PingForConfiguration>
-         <urn1:serviceContractNamespace>something</urn1:serviceContractNamespace>
-         <urn1:logicalAddress>5565594230</urn1:logicalAddress>
-      </urn1:PingForConfiguration>
-   </soapenv:Body>
-</soapenv:Envelope>
-
-<soap:Envelope 
-  xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
-  <soap:Body>
-    <PingForConfigurationResponse 
-      xmlns="urn:riv:itintegration:monitoring:PingForConfigurationResponder:1" 
-      xmlns:ns2="urn:riv:itintegration:registry:1">
-      <pingDateTime>20150226110001</pingDateTime>
-      <configuration>
-        <name>Applikation</name>
-        <value>GetAggregatedReferralOutcome</value>
-      </configuration>
-    </PingForConfigurationResponse>
-  </soap:Body>
-</soap:Envelope>
-*/
+}
